5fdd10cc4e0d82fbb92ffcf7db0d833ff63dddc0
Charan Reddy Katta <147185561+charanreddy9866@users.noreply.github.com>
2026-08-16 17:24:03 -0400

    Add user and onLogout props to Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 
-export default function Layout({ children }) {
+export default function Layout({ children, user, onLogout }) {
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
+    <div className="flex min-h-screen bg-dark-bg">
+      <Sidebar user={user} onLogout={onLogout} />
       
       {/* Main Content Area */}
       <main className="flex-1 lg:ml-0">
